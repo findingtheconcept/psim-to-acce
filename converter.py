949d5109059e48b1e77d60552c55fb6de7a38db4
Dmitriy Grape <grapeoff.official@gmail.com>
2025-02-20 00:27:29 +0300

    chore: clean-up code and fix filepaths

--- converter.py
+++ converter.py
@@ -1,7 +1,6 @@
 from openpyxl import load_workbook
-from fileinput import filename
 
-def Add_Pipes(sheet, data):
+def add_pipes(sheet, data):
     row = 2
 
     while data["B" + str(row)].value:
@@ -12,15 +11,16 @@ def Add_Pipes(sheet, data):
         sheet["C" + str(11+row-2)] = data["A" + str(row)].value
         sheet["O" + str(11+row-2)] = data["D" + str(row)].value
         sheet["P" + str(11+row-2)] = data["E" + str(row)].value
+
         row += 1
+
         print(row)
 
     return 0
 
-workbook = load_workbook(filename="C:\\Users\\roman\\Desktop\\small_export_pipes.xlsx")
-workbook2 = load_workbook(filename="C:\\Users\\roman\\Desktop\\toACCE.xlsx")
+workbook = load_workbook(filename="res/small_export_pipes.xlsx")
+workbook2 = load_workbook(filename="res/toACCE.xlsx")
 
+add_pipes(workbook['BPIPPIPE'], workbook2['Sheet2'])
 
-Add_Pipes(workbook['BPIPPIPE'], workbook2['Sheet2'])
-
-workbook.save(filename="../../Downloads/Telegram Desktop/Исходные материалы/Конвертор/export_spreedsheet.xlsx")
+workbook.save(filename="out/export_spreadsheet.xlsx")
