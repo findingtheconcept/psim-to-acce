dfe64513876eaacd32fcbce81fa313d1ab857a1f
Dmitriy Grape <grapeoff.official@gmail.com>
2025-02-27 22:57:03 +0300

    feat: design view for main window

--- src/app.py
+++ src/app.py
@@ -2,27 +2,46 @@ import logging
 import os
 from threading import Thread
 
+import requests
+from flask import Flask, render_template, request
 import webview
-from flask import Flask, render_template
 
 template_folder = os.path.join(os.getcwd(), 'assets')
+static_folder = os.path.join(os.getcwd(), 'static')
 
-app = Flask(__name__, template_folder=template_folder)
+app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
-app.config['DEBUG'] = True
-app.config['SERVER_NAME'] = '127.0.0.1:5000'
+def on_closed():
+    logger.info("Window closed. Stopping Flask server...")
+    try:
+        os.environ.pop('HTTP_PROXY', None)
+        os.environ.pop('HTTPS_PROXY', None)
+        proxies = {"http": None, "https": None}
+
+        requests.get("http://127.0.0.1:5000/shutdown", proxies=proxies, timeout=3)
+    except Exception as e:
+        logger.error(f"Error during server shutdown: {e}")
 
 @app.route('/')
 def index():
     return render_template('main_window.html')
 
+@app.route('/shutdown', methods=['GET'])
+def shutdown_route():
+    logger.info("Shutdown request received.")
+    shutdown_func = request.environ.get('werkzeug.server.shutdown')
+    if shutdown_func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    shutdown_func()
+    return "Server shutting down..."
+
 def run_flask():
+    logger.info("Running Flask back-end...")
     try:
-        logger.info("Running Flask back-end...")
-        app.run(debug=True, use_reloader=False)
+        app.run(host='127.0.0.1', port=5000, debug=False, use_reloader=False)
     except Exception as e:
         logger.error(f"Error caught during starting Flask back-end: {e}")
 
@@ -30,5 +49,11 @@ if __name__ == "__main__":
     thread = Thread(target=run_flask)
     thread.start()
 
-    webview.create_window('Конвертер PSIM (Excel) в ASSE', 'http://127.0.0.1:5000')
+    window = webview.create_window(
+        'PSIM to ASSE',
+        'http://127.0.0.1:5000',
+        width=1200,
+        height=500
+    )
+    window.events.closed += on_closed
     webview.start()
