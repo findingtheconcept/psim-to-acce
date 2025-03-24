da15f22c091b7d0f09ce8abd531f598ec4891e76
Dmitriy Grape <grapeoff.official@gmail.com>
2025-03-24 18:28:11 +0300

    feat: Add new screen for working with .ifc models and do some enhancements

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,5 @@
 import logging
 import os
-
 from flask import Blueprint, request, jsonify, render_template
 from converter import convert_psim_to_asse
 
@@ -13,17 +12,12 @@ def index():
 @bp.route('/convert', methods=['POST'])
 def convert_route():
     data = request.json
-
     psim_file = data.get('inputFile')
     second_file = data.get('secondFile')
-
     output_file = data.get('outputFile')
-
     logging.info(f"Convert from {psim_file} and {second_file} => {output_file}")
-
     if os.path.isdir(output_file):
         output_file = os.path.join(output_file, "export_spreadsheet.xlsx")
-
     try:
         convert_psim_to_asse(psim_file, second_file, output_file)
         return jsonify({"status": "success", "message": "Conversion complete!"})
